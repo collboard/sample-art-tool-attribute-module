68c6733d860f0774e3a30f0dbd510c2115985786
Pavol Hejny <me@pavolhejny.com>
2022-01-07 13:05:42 +0100

    Public profile of the module

--- src/SampleTool.tsx
+++ src/SampleTool.tsx
@@ -1,4 +1,6 @@
-import { declareModule, makeIconModuleOnModule, Registration, Separator, ToolbarName } from '@collboard/modules-sdk';
+// Note: We are using library destroyable and xyzt which is published under @hejny (creator of Collboard) but probbly it should be also published under @collboard to make clear that it is an integral part of Collboard stack.
+import { declareModule, makeIconModuleOnModule, Separator, ToolbarName } from '@collboard/modules-sdk';
+import { Registration } from 'destroyable';
 import * as React from 'react';
 import { contributors, description, license, repository, version } from '../package.json';
 import { SampleArt } from './SampleArt';
@@ -12,8 +14,9 @@ declareModule(
             contributors,
             license,
             repository,
-            title: { en: 'Drawing', cs: 'Kreslení' },
-            categories: ['Basic', 'Art'],
+            title: { en: 'Drawing of dotted and dashed lines', cs: 'Kreslení přerušovaných čar' },
+            categories: ['Basic', 'Art', 'Experimental'],
+            icon: '✒️',
             flags: {
                 isTemplate: true,
             },
